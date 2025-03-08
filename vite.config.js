1467c56ffd9035cb3ed403dff60454cca93a7ebf
Myptech <actaukee@gmail.com>
2025-03-08 12:02:19 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/lonelysitcher/",
+  base: "/lonelysitcher/", // Это важно для GitHub Pages!
 });
